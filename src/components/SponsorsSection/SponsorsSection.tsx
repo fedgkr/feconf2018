b548311a3214846dc3bf07e0d3a319056b96d010
U-Yeong Ju <uyeong21c@gmail.com>
2018-09-12 01:10:06 +0900

    update sponsor section

--- src/components/SponsorsSection/SponsorsSection.tsx
+++ src/components/SponsorsSection/SponsorsSection.tsx
@@ -1,66 +1,50 @@
 import cc from 'classcat';
-import { companyList, SupportLevel } from 'db/Company';
+import { companyList } from 'db/Company';
 import React from 'react';
 import { SponsorLogo } from './components/SponsorLogo/SponsorLogo';
 import css from './SponsorsSection.scss';
 
-interface SponsorsSectionProps {
-  appWidth: number;
-}
+const diamondSponsors = companyList.filter(c => c.isLevelDiamond());
+const platinumSponsors = companyList.filter(c => c.isLevelPlatinum());
+const goldSponsors = companyList.filter(c => c.isLevelGold());
 
-export class SponsorsSection extends React.Component<SponsorsSectionProps> {
-  public render() {
-    return (
-      <section className={css.SponsorsSection} id="sponsors">
-        <div className={css.Content}>
-          <h2 className={css.Title}>SPONSORS</h2>
-          <div className={css.Desc}>
-            스폰서의 소중한 후원과 여러분의 참여가 FEConf 2018를 만듭니다.<br />
-            고맙습니다.
-          </div>
-          <div className={css.SponsorsWrap}>
-            <div
-              className={cc({
-                [css.LevelWrap]: true,
-                [css.Diamond]: true,
-              })}
-            >
-              <h2 className={css.LevelTitle}>DIAMOND</h2>
-              <div className={css.SponsorList}>
-                {companyList.filter(c => c.level === SupportLevel.DIAMOND).map((c, idx) => (
-                  <SponsorLogo key={idx} company={c} />
-                ))}
-              </div>
-            </div>
-            <div
-              className={cc({
-                [css.LevelWrap]: true,
-                [css.Platinum]: true,
-              })}
-            >
-              <h2 className={css.LevelTitle}>PLATINUM</h2>
-              <div className={css.SponsorList}>
-                {companyList.filter(c => c.level === SupportLevel.PLATINUM).map((c, idx) => (
-                  <SponsorLogo key={idx} company={c} />
-                ))}
-              </div>
-            </div>
-            <div
-              className={cc({
-                [css.LevelWrap]: true,
-                [css.Gold]: true,
-              })}
-            >
-              <h2 className={css.LevelTitle}>GOLD</h2>
-              <div className={css.SponsorList}>
-                {companyList.filter(c => c.level === SupportLevel.GOLD).map((c, idx) => (
-                  <SponsorLogo key={idx} company={c} />
-                ))}
-              </div>
-            </div>
-          </div>
+const SponsorsSection: React.SFC = () => (
+  <section className={css.SponsorsSection} id="sponsors">
+    <div className={css.Content}>
+      <h2 className={css.Title}>SPONSORS</h2>
+      <div className={css.Desc}>
+        2018 FE 컨퍼런스 코리아는 스폰서들의 후원과
+        <br />
+        여러분의 참여로 개최될 수 있었습니다. 감사합니다.
+      </div>
+      <div className={css.SponsorsWrap}>
+        <div className={cc({ [css.LevelWrap]: true, [css.Diamond]: true })}>
+          <h3 className={css.LevelTitle}>DIAMOND</h3>
+          <ul className={css.SponsorList}>
+            {diamondSponsors.map((c, idx) => (
+              <SponsorLogo key={idx} company={c} />
+            ))}
+          </ul>
         </div>
-      </section>
-    );
-  }
-}
+        <div className={cc({ [css.LevelWrap]: true, [css.Platinum]: true })}>
+          <h3 className={css.LevelTitle}>PLATINUM</h3>
+          <ul className={css.SponsorList}>
+            {platinumSponsors.map((c, idx) => (
+              <SponsorLogo key={idx} company={c} />
+            ))}
+          </ul>
+        </div>
+        <div className={cc({ [css.LevelWrap]: true, [css.Gold]: true })}>
+          <h3 className={css.LevelTitle}>GOLD</h3>
+          <ul className={css.SponsorList}>
+            {goldSponsors.map((c, idx) => (
+              <SponsorLogo key={idx} company={c} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export { SponsorsSection };
