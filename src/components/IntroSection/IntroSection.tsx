e5bc736e877571b94c2e748d1d2fe3d4161973f7
Jooyoung Moon <hckrmoon@gmail.com>
2018-09-15 17:49:39 +0900

    map link added #13

--- src/components/IntroSection/IntroSection.tsx
+++ src/components/IntroSection/IntroSection.tsx
@@ -45,10 +45,12 @@ export class IntroSection extends React.Component {
                 <img src="static/images/pages/intro/calendar@2x.png" alt="Calendar" />
                 <p>2018년 11월 03일 토요일 10:00 - 17:00</p>
               </div>
-              <div className={css.Location}>
-                <img src="static/images/pages/intro/map@2x.png" alt="Map" />
-                <p>Lotte Tower 31F | 서울시 송파구 올림픽로 300</p>
-              </div>
+              <a target="_blank" href="http://naver.me/GF26VviE">
+                <div className={css.Location}>
+                  <img src="static/images/pages/intro/map@2x.png" alt="Map" />
+                  <p>Lotte Tower 31F | 서울시 송파구 올림픽로 300</p>
+                </div>
+              </a>
               <div className={css.ButtonWrap}>
                 <Button background="#a821d2" color="#fff">
                   사전 등록하기
