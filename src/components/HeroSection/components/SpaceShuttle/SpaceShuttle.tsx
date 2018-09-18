248a38b76c0bdf70d4a8290e28ce1d1dc9f3c381
Jooyoung Moon <hckrmoon@gmail.com>
2018-09-18 17:38:29 +0900

    IntersectionObserver applied to SpaceShuttle animation

--- src/components/HeroSection/components/SpaceShuttle/SpaceShuttle.tsx
+++ src/components/HeroSection/components/SpaceShuttle/SpaceShuttle.tsx
@@ -3,20 +3,41 @@ import cc from 'classcat';
 import css from './SpaceShuttle.scss';
 import { ArrayUtils } from '../../../../utils/ArrayUtils';
 
-export class SpaceShuttle extends React.Component<{}> {
-  public shouldComponentUpdate() {
-    return false;
+interface State {
+  active: boolean
+}
+
+export class SpaceShuttle extends React.Component<{}, State> {
+  public state = {
+    active: false,
+  }
+  private wrapper: HTMLElement;
+
+  public componentDidMount() {
+    const options = { threshold: 0, rootMargin: '-70px' };
+    const io = new IntersectionObserver(this.onIntersecting, options);
+    io.observe(this.wrapper);
+  }
+
+  public shouldComponentUpdate(_, nextState: State) {
+    return this.state.active !== nextState.active;
   }
 
   public render() {
+    const {active} = this.state
     return (
-      <div className={css.SpaceShuttle}>
+      <div className={css.SpaceShuttle} ref={el => (this.wrapper = el)}>
         <img className={css.Shuttle} src="static/images/pages/hero/shuttle.png" alt="Hero Image" />
         {ArrayUtils.createNArray(6).map(i => {
           const order = i + 1;
           const style = this.getRandomAnimationProp();
           return (
-            <div key={order} style={style} className={cc({ [css.Star]: true, [css[`Star${order}`]]: true })}>
+            <div key={order} style={style}
+                 className={cc({
+                   [css.Star]: true,
+                   [css[`Star${order}`]]: true,
+                   [css.active]: active,
+                 })}>
               <div className={css.Sprite} />
             </div>
           );
@@ -26,9 +47,16 @@ export class SpaceShuttle extends React.Component<{}> {
   }
 
   private getRandomAnimationProp() {
+    const {active} = this.state
     return {
-      animationDelay: `${Math.random()}s`,
-      animationDuration: `${1.5}s`,
+      transitionDelay: active ? `${Math.random()}s` : '0s',
+      transitionDuration: active ? `${1.5}s` : '0s',
     };
   }
+
+  private onIntersecting = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(({ isIntersecting }) => {
+      this.setState({active: isIntersecting});
+    });
+  };
 }
