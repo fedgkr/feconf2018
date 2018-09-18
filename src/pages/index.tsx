90ea9cf2b4c6809bc79b43bce2b437a6e5d9e3c0
Jooyoung Moon <hckrmoon@gmail.com>
2018-09-18 16:37:51 +0900

    font loading timing changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,6 +71,24 @@ class IndexPage extends React.Component<{}, IndexPageState> {
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link rel="icon" href="static/favicon.ico" />
           <link rel="stylesheet" href="static/styles/normalize.css" />
+          <style dangerouslySetInnerHTML={{__html: `
+            @font-face {
+                font-family: 'Spoqa Han Sans';
+                font-weight: 700;
+                src: local('Spoqa Han Sans Bold'),
+                url('static/fonts/SpoqaHanSans/SpoqaHanSansBold.woff2') format('woff2'),
+                url('static/fonts/SpoqaHanSans/SpoqaHanSansBold.woff') format('woff'),
+                url('static/fonts/SpoqaHanSans/SpoqaHanSansBold.ttf') format('truetype');
+            }
+            @font-face {
+                font-family: 'Spoqa Han Sans';
+                font-weight: 400;
+                src: local('Spoqa Han Sans Regular'),
+                url('static/fonts/SpoqaHanSans/SpoqaHanSansRegular.woff2') format('woff2'),
+                url('static/fonts/SpoqaHanSans/SpoqaHanSansRegular.woff') format('woff'),
+                url('static/fonts/SpoqaHanSans/SpoqaHanSansRegular.ttf') format('truetype');
+            }
+          `}} />
           <script
             dangerouslySetInnerHTML={{
               __html: `
