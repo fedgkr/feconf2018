1edc12972cb462c5082d979e404f1501a84cc385
codemilli <codemilli@webtoonscorp.com>
2018-10-28 21:26:04 +0900

    rm useless style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,11 @@
-import { readFileSync } from 'fs';
 import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
-
-let version = '';
-
-if (process.env.NODE_ENV === 'production') {
-  version = `?v=${readFileSync(`${process.cwd()}/.next/BUILD_ID`)}`;
-}
 
 export default class extends Document {
   public render() {
     return (
       <html>
-        <Head>
-          <link rel="stylesheet" href={`_next/static/style.css${version}`} />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
